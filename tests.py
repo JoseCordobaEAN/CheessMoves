a740bd3c89cbe4a152bac8807a8cdaae13386dbb
Jose <jmcordoba@universidadean.edu.co>
2019-05-07 19:06:04 -0500

    Agregada la definicion de la prueba mover torre

--- tests.py
+++ tests.py
@@ -14,3 +14,9 @@ class Test_movimientos(TestCase):
         espero = ""
         obtengo = obtener_nombre_pieza(dado)
         self.assertEquals(espero, obtengo)
+
+    def test_mover_torre(self):
+        dado = []
+        espero = []
+        obtengo = mover_torre(dado[0], dado[1], dado[2] , dado[3], dado[4])
+        self.assertEquals(espero, obtengo)
