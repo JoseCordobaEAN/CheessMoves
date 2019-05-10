f47c9c87f0747ad8f0372676ebcc8e9ef7259d8c
Jose <jmcordoba@universidadean.edu.co>
2019-05-09 19:09:27 -0500

    Agregado el movimiento de la torre

--- movimientos.py
+++ movimientos.py
@@ -1,3 +1,5 @@
+import sys
+
 def tablero_a_cadena(tablero):
     cadena = ""
     for fila in tablero:
@@ -40,4 +42,51 @@ def obtener_nombre_pieza(simbolo):
 
 
 def mover_torre(tablero, x_inicial, y_inicial, x_final, y_final):
-    pass
+    # Valido que se este moviendo una torre
+    if tablero[y_inicial][x_inicial].lower() == 't':
+
+        # Valido que se este moviendo en y
+        if x_inicial == x_final and y_inicial != y_final:
+
+            # Si me muevo hacia abajo
+            if y_inicial < y_final:
+                y_auxiliar = y_inicial + 1
+            # Si no me muevo hacia abajo
+            else:
+                y_auxiliar = y_inicial - 1
+            # Reviso  el camino por obstaculos
+            for y in range(y_auxiliar, y_final):
+                if tablero[y][x_inicial] != ' ':
+                    raise Exception('No hay camino para mover la torre')
+
+
+
+
+
+        # Valido el movimiento en x
+        elif x_inicial != x_final and y_inicial == y_final:
+            # validar si me muevo a la derecha
+            if x_inicial < x_final:
+                x_auxiliar = x_inicial + 1
+            else:
+                x_auxiliar = x_inicial - 1
+
+            # Reviso el camino por obtaculos
+            for x in range(x_auxiliar, x_final):
+                if tablero[x][y_inicial] != ' ':
+                    raise Exception('No hay camino para mover la torre')
+
+
+        else:
+            raise Exception('Movimiento invalido para la torre')
+
+        if tablero[y_final][x_inicial] == ' ' \
+                or (tablero[y_inicial][x_inicial].islower() != tablero[y_final][x_inicial].islower()):
+            tablero[y_final][x_inicial] = tablero[y_inicial][x_inicial]
+            tablero[y_inicial][x_inicial] = ' '
+        else:
+            raise Exception('No puedo comer mis propias piezas')
+
+    else:
+        raise Exception('La pieza en x = {0} y={1} no es una torre'.format(x_inicial,y_inicial))
+    return tablero
--- tests.py
+++ tests.py
@@ -16,7 +16,34 @@ class Test_movimientos(TestCase):
         self.assertEquals(espero, obtengo)
 
     def test_mover_torre(self):
-        dado = []
-        espero = []
-        obtengo = mover_torre(dado[0], dado[1], dado[2] , dado[3], dado[4])
-        self.assertEquals(espero, obtengo)
+        dado = [[
+['t', 'k', 'a', 'q', 'r', 'a', 'k', 't'],
+['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+['T', 'K', 'A', 'R', 'Q', 'A', 'K', 'T']
+],0, 7, 0, 2 ] # lista con los parametros a usar en la función
+        espero = [
+['t', 'k', 'a', 'q', 'r', 'a', 'k', 't'],
+['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+['T', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+[' ', 'K', 'A', 'R', 'Q', 'A', 'K', 'T']
+]
+
+
+
+        print("Dado\n",
+              tablero_a_cadena(dado[0]), dado[1], dado[2] , dado[3], dado[4])
+        print("espero\n",
+              tablero_a_cadena(espero))
+        obtengo = mover_torre(dado[0], dado[1], dado[2], dado[3], dado[4])
+        print("obtengo\n",
+              tablero_a_cadena(obtengo))
+        self.assertEqual(espero, obtengo)
